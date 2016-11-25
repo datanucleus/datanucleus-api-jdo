c8160b970fb6cbda08784b3edde15c3ff1900982
andyjefferson <andy@datanucleus.org>
2016-11-25 07:30:15 +0000

    Add extension annotations for @CreateTimestamp/@UpdateTimestamp that create internal metadata requiring field to store create/update timestamp. Also can be used for a surrogate column in the future. Fixes #40.

---
+++ src/main/java/org/datanucleus/api/jdo/annotations/UpdateTimestampHandler.java
@@ -0,0 +1,40 @@
+/**********************************************************************
+Copyright (c) 2016 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
+package org.datanucleus.api.jdo.annotations;
+
+import org.datanucleus.ClassLoaderResolver;
+import org.datanucleus.metadata.AbstractClassMetaData;
+import org.datanucleus.metadata.AbstractMemberMetaData;
+import org.datanucleus.metadata.annotations.AnnotationObject;
+import org.datanucleus.metadata.annotations.MemberAnnotationHandler;
+
+/**
+ * Handler for the {@link UpdateTimestamp} annotation when applied to a field/property or surrogate column of a persistable class.
+ */
+public class UpdateTimestampHandler implements MemberAnnotationHandler
+{
+    public void processMemberAnnotation(AnnotationObject ann, AbstractMemberMetaData mmd, ClassLoaderResolver clr)
+    {
+        mmd.addExtension("update-timestamp", "true");
+    }
+
+    public void processClassAnnotation(AnnotationObject annotation, AbstractClassMetaData cmd, ClassLoaderResolver clr)
+    {
+        cmd.addExtension("update-timestamp", "true");
+    }
+}
