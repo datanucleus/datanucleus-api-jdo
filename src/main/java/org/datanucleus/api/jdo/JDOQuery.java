6602c794562e5e539211d18aa765ffc5eab73179
andyjefferson <andy@datanucleus.org>
2017-05-31 07:05:56 +0100

    Backport of api-jdo-52. Fixes #54

--- src/main/java/org/datanucleus/api/jdo/JDOQuery.java
+++ src/main/java/org/datanucleus/api/jdo/JDOQuery.java
@@ -367,10 +367,11 @@ public class JDOQuery<T> implements Query<T>
     public <R> List<R> executeResultList(Class<R> resultCls)
     {
         assertIsOpen();
-        if (query.getResult() == null)
+        if (resultCls == null)
         {
-            throw new JDOUserException("Cannot call executeResultList method when query has result unset. Call executeList instead.");
+            throw new JDOUserException("Result Class must be specified");
         }
+
         this.query.setResultClass(resultCls);
         return (List<R>) executeInternal();
     }
@@ -383,10 +384,11 @@ public class JDOQuery<T> implements Query<T>
     {
         assertIsOpen();
         query.setUnique(true);
-        if (query.getResult() == null)
+        if (resultCls == null)
         {
-            throw new JDOUserException("Cannot call executeResultUnique method when query has result unset. Call executeUnique instead.");
+            throw new JDOUserException("Result Class must be specified");
         }
+
         this.query.setResultClass(resultCls);
         return (R) executeInternal();
     }
