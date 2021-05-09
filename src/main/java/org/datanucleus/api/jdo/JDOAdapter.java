ca1964a2eeebc4ed73e9dfdd0af3fc5ed57bc901
andyjefferson <andy@datanucleus.org>
2021-05-09 18:24:41 +0100

    Add accessor for printing out class metadata. Fixes #110

--- src/main/java/org/datanucleus/api/jdo/JDOAdapter.java
+++ src/main/java/org/datanucleus/api/jdo/JDOAdapter.java
@@ -36,6 +36,7 @@ import org.datanucleus.ClassNameConstants;
 import org.datanucleus.ExecutionContext;
 import org.datanucleus.PropertyNames;
 import org.datanucleus.api.ApiAdapter;
+import org.datanucleus.api.jdo.metadata.JDOMetaDataHelper;
 import org.datanucleus.api.jdo.state.LifeCycleStateFactory;
 import org.datanucleus.enhancement.Persistable;
 import org.datanucleus.exceptions.NucleusException;
@@ -130,6 +131,12 @@ public class JDOAdapter implements ApiAdapter
             return true;
         }
         return false;
+    }
+
+    @Override
+    public String getXMLMetaDataForClass(AbstractClassMetaData cmd, String prefix, String indent)
+    {
+        return new JDOMetaDataHelper().getXMLForMetaData(cmd, prefix, indent);
     }
 
     // ------------------------------ Object Lifecycle --------------------------------
